059fbcbca7ca3b45fa856ddbb72b140f5b26c32e
Anthony PINEAU <anthonypineau85@gmail.com>
2022-11-18 00:02:19 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,153 +1,147 @@
+const listBody = document.querySelector("#list .body");
+const cartBody = document.querySelector("#cart .body");
 
+const cities = new Array;
+const trainings = new Array;
+let filtered = new Array;
+let selectedTrainings = new Array;
 
-
-
-const listeBP = document.querySelector("#listeBP");
-const listeBPChecked = new Array();
-const phases = ["Aucune phase", "Administration", "Conception", "Réalisation", "Acquisition", "Déploiement", "Utilisation", "Maintenance", "Fin de vie", "Revalorisation"];
+var map = L.map('map').setView([46.861, 2.131], 6);
+L.tileLayer('../../assets/osm/{z}/{x}/{y}.png', {
+    minZoom : 6,
+    maxZoom: 6,
+    noWrap: true,
+    bounds: [
+      [51.454, -5.405],
+      [42.115, 8.262]
+    ],
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 fetch("../../data/data.json")
 .then(response => {
    return response.json();
 }).then(data => {
     data.forEach(d => {
-      const div = document.createElement("div");
-      div.classList.add("card");
-      div.classList.add(d.category)
-      div.textContent=d.name;
-      if(d.incontournable){
-        div.classList.add("incontournable");
-        div.classList.add("selected");
-        listeBPChecked.push(d);
-      }else{
-        div.classList.add("contournable");
-        div.addEventListener("click", () => {
-            if(div.classList.contains("selected")){
-                div.classList.remove("selected");
-                if(document.querySelector(".active").id=="selected"){
-                  filterSelection("selected");
-                }
-                listeBPChecked.splice(listeBPChecked.indexOf(d), 1);
-            }else{
-                div.classList.add("selected");
-                listeBPChecked.push(d);
-            }
-        });
+      trainings.push(d);
+      if(!cities.includes(d.city)){
+        cities.push(d.city);
       }
-      listeBP.appendChild(div);
     });
-    filterSelection("all");
+    filtered = trainings;
+    const selectCity = document.querySelector("#city");
+    cities.forEach(c => {
+      const optCity = document.createElement("option");
+      optCity.value = c;
+      optCity.text = c;
+      selectCity.appendChild(optCity);
+    });
+
+    renderTrainings();
 });
 
-function filterSelection(filter) {
-  const cards = document.querySelectorAll(".card");
-  if (filter == "all") filter = "card";
-  cards.forEach((c) => {
-    c.classList.remove("show");
-    if (c.classList.contains(filter)){
-      c.classList.add("show");
-    }
-  });
+const pdf = document.querySelector("#pdf");
+pdf.addEventListener("click", () => {
+  var mywindow = window.open("", "PRINT", "height=600,width=600");
+  mywindow.document.write(cartBody.innerHTML);
+  mywindow.document.close();
+  mywindow.focus();
+  mywindow.print();
+  return true;
+});
+
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", e => {
+  filter = {
+    "formation" : selectTrainingType.value,
+    "organisation" : selectOrganistion.value,
+    "city": selectCity.value,
+    "access": selectAccess.value
+  }
+
+  filtered = trainings.filter(obj => (filter.formation == 'null' ? true : obj.formation == filter.formation) && (filter.organisation == 'null' ? true : obj.organisation == filter.organisation) && (filter.city == 'null' ? true : obj.city == filter.city) && (filter.access == 'null' ? true : obj.access == filter.access) );
+
+  renderTrainings();
+  renderCart();
+});
+
+function createCard(training){
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.id = training.name;
+  div.textContent=training.name;
+  return div;
 }
 
-document.querySelectorAll(".submit").forEach((i) => {
-  i.addEventListener("click", () => {
-    const divTable = document.querySelector(".table");
-    divTable.innerHTML="";
-    const button = document.createElement("button");
-    button.textContent="Télécharger le fichier";
-    button.addEventListener("click", () => {
-      const csv = [];
-      const rows = document.querySelectorAll("table tr");
-      rows.forEach((r) => {
-        const row = [], cols = r.querySelectorAll("td, th");
-        cols.forEach((c) => {
-          row.push(c.innerText);
-        }); 
-        csv.push(row.join(";"));
-      });
-      const downloadLink = window.document.createElement('a');
-      const universalBOM = "\uFEFF";
-      downloadLink.setAttribute('href', 'data:text/csv; charset=utf-8,' + encodeURIComponent(universalBOM+csv.join("\n")));
-      downloadLink.setAttribute('download', 'gantt.csv');
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-    });
-    button.classList.add("button");
-    divTable.appendChild(button);
-    const table = document.createElement("table");
-    phases.forEach((p) => {
-      const thead = document.createElement("thead");
-      const trhead = document.createElement("tr");
-      const thPhase = document.createElement("th");
-      const thStep = document.createElement("th");
-      const thIndicateur = document.createElement("th");
-      
+function renderTrainings(){
+  listBody.innerHTML="";
+  
+  filtered.forEach(t => {
+    const div = createCard(t);
 
-      thPhase.textContent=p;
-      thStep.textContent="Étapes clés";
-      thIndicateur.textContent="Indicateurs";
-      
+    let marker = L.marker(t.latlong).addTo(map).bindPopup(t.name);
+    div.addEventListener("mouseover", e => {
+      marker.openPopup();
+    });
 
-      trhead.appendChild(thPhase);
-      trhead.appendChild(thStep);
-      trhead.appendChild(thIndicateur);
-      thead.appendChild(trhead);
-      table.appendChild(thead);
-      const tbody = document.createElement("tbody");
-      const listeBPCheckedFilter = listeBPChecked.filter((e) => { return p==e.lifecycle; });
-      listeBPCheckedFilter.forEach((i) => {
-        const trbody = document.createElement("tr");
-        const tdPhase = document.createElement("td");
-        const tdStep = document.createElement("td");
-        const tdIndicateur = document.createElement("td");
-        
-        tdPhase.textContent=i.name;
-        tdStep.textContent=i.keystep;
-        if(i.indicator!=""){
-          tdIndicateur.textContent=i.indicator+", x : "+i.indicatorX+", y :"+i.indicatorY;
-        }else{
-          tdIndicateur.textContent="Aucuns indicateurs";
-        }
-        
-        trbody.appendChild(tdPhase);
-        trbody.appendChild(tdStep);
-        trbody.appendChild(tdIndicateur);
-        tbody.appendChild(trbody);
-      });
-      table.appendChild(tbody);
+    div.addEventListener("mouseout", e => {
+      marker.closePopup();
     });
-    divTable.appendChild(table);
-    const button2 = document.createElement("button");
-    button2.textContent="Télécharger le fichier";
-    button2.addEventListener("click", () => {
-      const csv = [];
-      const rows = document.querySelectorAll("table tr");
-      rows.forEach((r) => {
-        const row = [], cols = r.querySelectorAll("td, th");
-        cols.forEach((c) => {
-          row.push(c.innerText);
-        }); 
-        csv.push(row.join(";"));
+
+    div.addEventListener("click", () => {
+      const divCart = createCard(t);
+      divCart.addEventListener("click", () => {
+        cartBody.removeChild(divCart);
+        div.style.display = "block";
+        selectedTrainings = selectedTrainings.slice(selectedTrainings.indexOf(t));
       });
-      const downloadLink = window.document.createElement('a');
-      const universalBOM = "\uFEFF";
-      downloadLink.setAttribute('href', 'data:text/csv; charset=utf-8,' + encodeURIComponent(universalBOM+csv.join("\n")));
-      downloadLink.setAttribute('download', 'gantt.csv');
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
+
+      selectedTrainings.push(t);
+
+      cartBody.appendChild(divCart);
+
+      div.style.display = "none";
     });
-    button2.classList.add("button");
-    divTable.appendChild(button2);
+
+    marker.addEventListener("click", () => {
+      if(marker.isPopupOpen()){
+        window.location = "#"+t.name;
+      } 
+    });
+
+    marker.addEventListener("popupopen", () => {
+      div.classList.add("shine");
+    });
+
+    marker.addEventListener("popupclose", () => {
+      div.classList.remove("shine");
+    });
+
+    submit.addEventListener("click", e => {
+      map.removeLayer(marker);
+    });
+
+    listBody.appendChild(div);
+  });     
+}
+
+const selectTrainingType = document.querySelector("#trainingType");
+const selectOrganistion = document.querySelector("#organisation");
+const selectCity = document.querySelector("#city");
+const selectAccess = document.querySelector("#access");
+
+const namePerson = document.querySelector("#namePerson");
+
+const mail = document.querySelector("#mail");
+mail.addEventListener("click", () => {
+  let yourMessage = "Liste de formations : ";
+  selectedTrainings.forEach(t => {
+    yourMessage += t.name + ", ";
   });
-});
+  let subject = "Demande de devis - "+ namePerson.value;
 
-const btns = document.querySelectorAll("#myBtnContainer .btn");
-btns.forEach((b) => {
-    b.addEventListener("click", function() {
-        const current = document.querySelector(".active");
-        current.classList.remove("active");
-        this.classList.add("active");
-        filterSelection(b.id);
-      });
+  document.location.href = "mailto:design4green@etik.com?subject="
+      + encodeURIComponent(subject)
+      + "&body=" + encodeURIComponent(yourMessage);
+
 });
